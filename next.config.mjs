9f2cc14bbb0ef2508bd4c1d67fe2498c9efb0839
Bunyod Yoqubjonov <bunyod.yoqubjonov@finnetlimited.com>
2026-05-30 11:27:59 +0500

    Deploy to new server with ci-cd

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: 'standalone', // Docker uchun optimallashtirilgan
 };
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.js");
